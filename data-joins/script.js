f068249500e54807d08e83036ff626524e397dba
Lucia Cyprianova <l.cyprianova@gmail.com>
2020-11-19 15:17:45 +0100

    finish data joins

--- data-joins/script.js
+++ data-joins/script.js
@@ -44,3 +44,46 @@ s.attr("cx", function (d, i) {
 s.classed("high", function (d) {
   return d >= 40; // returns true or false
 });
+
+// 03
+let cities = [
+  { name: "London", population: 8674000 },
+  { name: "New York", population: 8406000 },
+  { name: "Sydney", population: 4293000 },
+  { name: "Paris", population: 2244000 },
+  { name: "Beijing", population: 11510000 },
+];
+
+let s3 = d3.select(".data3").selectAll("circle");
+s3.data(cities);
+s3.attr("r", function (d) {
+  var scaleFactor = 0.000005;
+  return d.population * scaleFactor;
+}).attr("cx", function (d, i) {
+  return i * 120;
+});
+
+// 04
+d3.select(".data4")
+  .selectAll("rect")
+  .data(cities)
+  .attr("height", 19)
+  .attr("width", function (d) {
+    var scaleFactor = 0.00004;
+    return d.population * scaleFactor;
+  })
+  .attr("y", function (d, i) {
+    return i * 20;
+  });
+
+// Join cities to text elements and modify content and position
+d3.select(".data4")
+  .selectAll("text")
+  .data(cities)
+  .attr("y", function (d, i) {
+    return i * 20 + 13;
+  })
+  .attr("x", -5)
+  .text(function (d) {
+    return d.name;
+  });
